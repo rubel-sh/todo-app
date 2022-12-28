e0359ef7be1622632ae14939b78e5b562b6bb37b
Rubel Hossain <rubrex96@hotmail.com>
2022-12-29 00:35:16 +0600

    Footer added

--- client/todo-app/src/pages/shared/TodoFooter/TodoFooter.jsx
+++ client/todo-app/src/pages/shared/TodoFooter/TodoFooter.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const TodoFooter = () => {
-  return <div className="text-center">TodoAPP</div>;
+  return (
+    <div className="text-center mt-20 p-5">
+      Designed and Developed by <Wrap>Rubel Hossain</Wrap>.
+      <br /> Built with <Wrap>React.js</Wrap>, <Wrap>Redux</Wrap>,{" "}
+      <Wrap>Firebase</Wrap> and <Wrap>Tailwind</Wrap>. Hosted on{" "}
+      <Wrap>Netlify</Wrap>.
+    </div>
+  );
 };
 
 export default TodoFooter;
+
+const Wrap = ({ children }) => {
+  return (
+    <span className="text-primaryColor text-sm font-bold">{children}</span>
+  );
+};
